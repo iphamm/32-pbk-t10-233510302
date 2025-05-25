9248c14ef7f0bbb163396469b193f12696608965
M. Ilham Ramadhana <milhamramadhana25@gmail.com>
2025-05-25 22:52:48 +0700

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app" class="app-layout">
+  <div class="page-container">
+    <!-- Header -->
     <Header />
-    <div class="body-layout">
-      <Sidebar />
+
+    <!-- Konten Utama dengan Sidebar dan Konten -->
+    <div class="main-layout">
+      <aside class="sidebar">
+        <Sidebar />
+      </aside>
+
       <main class="main-content">
         <router-view />
       </main>
     </div>
+
+    <!-- Footer -->
     <Footer />
   </div>
 </template>
@@ -17,22 +25,34 @@ import Sidebar from './components/Sidebar.vue'
 import Footer from './components/Footer.vue'
 </script>
 
-<style scoped>
-.app-layout {
+<style scoped>.page-container {
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 
-.body-layout {
+.main-layout {
   display: flex;
   flex: 1;
   overflow: hidden;
 }
 
+.sidebar {
+  width: 250px;
+  background-color: #375689;
+  color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 .main-content {
-  flex-grow: 1;
+  flex: 1; 
+  background-color: #111827;
+  color: white;
   padding: 20px;
   overflow-y: auto;
+  min-width: 0; 
 }
 </style>
